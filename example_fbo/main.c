f7e5ae9a84b3d9e7ab5029489fbfc5cc15906c6f
Danil <s48gs.w@gmail.com>
2020-04-06 00:52:38 +0300

    fix

--- example_fbo/main.c
+++ example_fbo/main.c
@@ -314,7 +314,7 @@ if(!load_once){
 		[0] = {
 			.location = 0,
 			.binding = 0,
-			.format = VK_FORMAT_R32G32B32_SFLOAT,
+			.format = VK_FORMAT_R32G32_SFLOAT,
 			.offset = 0,
 		},
 	};
@@ -403,7 +403,7 @@ if(!load_once){
 		[0] = {
 			.location = 0,
 			.binding = 0,
-			.format = VK_FORMAT_R32G32B32_SFLOAT,
+			.format = VK_FORMAT_R32G32_SFLOAT,
 			.offset = 0,
 		},
 	};
