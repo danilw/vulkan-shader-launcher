0cef837a18caf8964707f59754b0db59b5aee7d8
Danil <s48gs.w@gmail.com>
2021-03-05 21:12:23 +0300

    small fix

--- use_validation_layer/VK_validation.sh
+++ use_validation_layer/VK_validation.sh
@@ -3,11 +3,7 @@
 VULKAN_SDK="<path to Vulkan SDK/version>" #example /home/user/vulkan_sdk/1.2.131.2/
 VK_APP=$1
 
-## bash and dash
-if which source >/dev/null; then 
-	source $VULKAN_SDK"/setup-env.sh"
-else
-	. $VULKAN_SDK"/setup-env.sh"
-fi
+. $VULKAN_SDK"/setup-env.sh"
+
 export VK_INSTANCE_LAYERS=VK_LAYER_KHRONOS_validation
 ./$VK_APP
