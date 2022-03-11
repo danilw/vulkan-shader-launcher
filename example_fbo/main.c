7a7aa96413bcc22d172fca723ab96131f891568c
Danil <s48gs.w@gmail.com>
2022-03-11 15:55:32 +0300

    Update main.c

--- example_fbo/main.c
+++ example_fbo/main.c
@@ -997,6 +997,7 @@ int render_index = 0;
 static bool render_loop_draw(struct vk_physical_device *phy_dev, struct vk_device *dev, struct vk_swapchain *swapchain,
                              struct app_os_window *os_window)
 {
+    if (!os_window->prepared) return true;
     int res;
     vk_error retval = VK_ERROR_NONE;
 
